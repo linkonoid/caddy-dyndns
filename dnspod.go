cff0f21114a7085f123e257f0d810f30ba903458
奶爸 <hi@nai.ba>
2019-05-11 18:17:34 +0800

    :rocket: work done but waitting test online

--- dnspod.go
+++ dnspod.go
@@ -10,29 +10,28 @@ import (
 	"strings"
 )
 
-type dnspodRecord struct {
-	Name   string
-	Line   string
-	Type   string
-	TTL    string
-	Value  string
-	MX     string
-	Status string
+type dnspodRecordResp struct {
+	Name   string `json:"name,omitempty"`
+	Line   string `json:"line,omitempty"`
+	Type   string `json:"type,omitempty"`
+	TTL    string `json:"ttl,omitempty"`
+	Value  string `json:"value,omitempty"`
+	Status string `json:"status,omitempty"`
 }
 
 type dnspodResp struct {
 	Status struct {
-		Code string
-	}
-	Records []dnspodRecord
+		Code string `json:"code,omitempty"`
+	} `json:"status,omitempty"`
+	Records []dnspodRecordResp `json:"records,omitempty"`
 }
 
 func dnspodupd(configs Config) error {
 
 	// Switch API endpoint
-	var dnspodAPIBase, tokenName = "https://dnspod.cn/", "login_token"
+	var dnspodAPIBase, tokenName, defaultRecordLine = "https://dnspod.cn/", "login_token", "默认"
 	if configs.Auth.Email == "international@dnspod.com" {
-		dnspodAPIBase, tokenName = "https://api.dnspod.com/", "user_token"
+		dnspodAPIBase, tokenName, defaultRecordLine = "https://api.dnspod.com/", "user_token", "default"
 	}
 
 	for _, domainName := range configs.Domains {
@@ -51,7 +50,37 @@ func dnspodupd(configs Config) error {
 		v.Set("record_type", "A")
 		body := ioutil.NopCloser(strings.NewReader(v.Encode()))
 		// build request
-		resp, err := respdnspodGetResp(body)
+		resp, err := respdnspodGetResp("Record.List", body)
+		if err != nil {
+			return err
+		}
+		var resp dnspodResp
+		err = json.Unmarshal(body, &resp)
+		if err != nil {
+			return err
+		}
+		if resp.Status.Code != "1" {
+			return errors.New("Errcode: " + resp.Status.Code)
+		}
+		// create or modify dnspod record
+		var apiType = "Record.Modify"
+		v := url.Values{}
+		v.Set("domain", domain)
+		v.Set("sub_domain", subDomain)
+		v.Set("record_type", "A")
+		if len(resp.Records) == 0 {
+			apiType = "Record.Create"
+			v.Set("record_line", defaultRecordLine)
+			v.Set("value", configs.Ipupdate)
+		} else if resp.Records[0].Value != configs.Ipupdate {
+			v.Set("record_line", defaultRecordLine)
+			v.Set("value", configs.Ipupdate)
+			v.Set("status", resp.Record[0].Status)
+		} else {
+			return nil
+		}
+		body := ioutil.NopCloser(strings.NewReader(v.Encode()))
+		resp, err := respdnspodGetResp(apiType, body)
 		if err != nil {
 			return err
 		}
@@ -68,9 +97,9 @@ func dnspodupd(configs Config) error {
 	return nil
 }
 
-func dnspodGetResp(body io.ReadCloser) (string, error) {
+func dnspodGetResp(endpoint string, body io.ReadCloser) (string, error) {
 	client := &http.Client{}
-	req, err := http.NewRequest("POST", dnspodAPIBase+"Record.List", body)
+	req, err := http.NewRequest("POST", dnspodAPIBase+endpoint, body)
 	if err != nil {
 		return "", err
 	}
