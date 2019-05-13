2339185e8cb3ce71a89bf5e8b1f2a34fdabacc2e
奶爸 <hi@nai.ba>
2019-05-13 13:15:29 +0800

    :tada: Test online pass

--- dnspod.go
+++ dnspod.go
@@ -5,10 +5,18 @@ import (
 	"errors"
 	"io"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"net/url"
+	"os"
 	"strings"
 )
+
+var debug bool
+
+func init() {
+	debug = os.Getenv("DNSPOD_DEBUG") == "on"
+}
 
 type dnspodRecordResp struct {
 	Name   string `json:"name,omitempty"`
@@ -17,6 +25,7 @@ type dnspodRecordResp struct {
 	TTL    string `json:"ttl,omitempty"`
 	Value  string `json:"value,omitempty"`
 	Status string `json:"status,omitempty"`
+	ID     string `json:"id,omitempty"`
 }
 
 type dnspodResp struct {
@@ -27,9 +36,8 @@ type dnspodResp struct {
 }
 
 func dnspodupd(configs Config) error {
-
 	// Switch API endpoint
-	var dnspodAPIBase, tokenName, defaultRecordLine = "https://dnspod.cn/", "login_token", "默认"
+	var dnspodAPIBase, tokenName, defaultRecordLine = "https://dnsapi.cn/", "login_token", "默认"
 	if configs.Auth.Email == "international@dnspod.com" {
 		dnspodAPIBase, tokenName, defaultRecordLine = "https://api.dnspod.com/", "user_token", "default"
 	}
@@ -39,35 +47,37 @@ func dnspodupd(configs Config) error {
 		domain := domainArray[len(domainArray)-2] + "." + domainArray[len(domainArray)-1]
 		var subDomain string
 		if len(domainArray) > 2 {
-			subDomain = domainName[:len(domainName)-len(domain)]
+			subDomain = domainName[:len(domainName)-len(domain)-1]
 		}
 		// get domain record
 		v := url.Values{}
 		v.Set("domain", domain)
-		v.Set(tokenName, configs.Auth.Apikey)
-		v.Set("length", "1")
 		v.Set("sub_domain", subDomain)
+		v.Set("format", "json")
 		v.Set("record_type", "A")
-		body := ioutil.NopCloser(strings.NewReader(v.Encode()))
+		v.Set(tokenName, configs.Auth.Apikey)
+		if debug {
+			log.Println(v)
+		}
+		data := ioutil.NopCloser(strings.NewReader(v.Encode()))
 		// build request
-		resp, err := respdnspodGetResp("Record.List", body)
+		bBody, err := dnspodGetResp(dnspodAPIBase+"Record.List", data)
 		if err != nil {
 			return err
 		}
 		var resp dnspodResp
-		err = json.Unmarshal(body, &resp)
+		err = json.Unmarshal(bBody, &resp)
 		if err != nil {
 			return err
 		}
-		if resp.Status.Code != "1" {
-			return errors.New("Errcode: " + resp.Status.Code)
-		}
 		// create or modify dnspod record
 		var apiType = "Record.Modify"
-		v := url.Values{}
+		v = url.Values{}
 		v.Set("domain", domain)
 		v.Set("sub_domain", subDomain)
+		v.Set("format", "json")
 		v.Set("record_type", "A")
+		v.Set(tokenName, configs.Auth.Apikey)
 		if len(resp.Records) == 0 {
 			apiType = "Record.Create"
 			v.Set("record_line", defaultRecordLine)
@@ -75,17 +85,20 @@ func dnspodupd(configs Config) error {
 		} else if resp.Records[0].Value != configs.Ipupdate {
 			v.Set("record_line", defaultRecordLine)
 			v.Set("value", configs.Ipupdate)
-			v.Set("status", resp.Record[0].Status)
+			v.Set("status", resp.Records[0].Status)
+			v.Set("record_id", resp.Records[0].ID)
 		} else {
 			return nil
 		}
-		body := ioutil.NopCloser(strings.NewReader(v.Encode()))
-		resp, err := respdnspodGetResp(apiType, body)
+		if debug {
+			log.Println(v)
+		}
+		data = ioutil.NopCloser(strings.NewReader(v.Encode()))
+		bBody, err = dnspodGetResp(dnspodAPIBase+apiType, data)
 		if err != nil {
 			return err
 		}
-		var resp dnspodResp
-		err = json.Unmarshal(body, &resp)
+		err = json.Unmarshal(bBody, &resp)
 		if err != nil {
 			return err
 		}
@@ -97,22 +110,27 @@ func dnspodupd(configs Config) error {
 	return nil
 }
 
-func dnspodGetResp(endpoint string, body io.ReadCloser) (string, error) {
+func dnspodGetResp(url string, body io.ReadCloser) ([]byte, error) {
 	client := &http.Client{}
-	req, err := http.NewRequest("POST", dnspodAPIBase+endpoint, body)
+	req, err := http.NewRequest("POST", url, body)
+	// req, err := http.NewRequest("POST", "https://httpbin.org/anything", body)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-	req.Header.Set("UserAgent", "caddy-ddns/0.0.1 (hi@hai.ba)")
+	req.Header.Set("User-Agent", "caddy-ddns/0.0.1 (hi@hai.ba)")
 	resp, err := client.Do(req)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
+	bBody, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
-	return body, nil
+	if debug {
+		log.Println(string(bBody))
+		log.Println(url)
+	}
+	return bBody, nil
 }
